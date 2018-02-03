b9a7fcdfaed652b1f8c66fd7c3f5f19d2e94a5ab
Madhu Sudhan <madhuchary80@gmail.com>
2018-02-03 05:35:26 +0000

    Openresty task - added python-pip to ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -9,7 +9,7 @@
 
 echo "Installing Ansible"
 sudo apt-get update
-sudo apt-get install software-properties-common -y
+sudo apt-get install python-pip software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y 
